d6d29d3e2c2022a5ccc925ca89014166bb6b5c73
sorryisme <wivipp39@gmail.com>
2018-03-17 23:32:13 +0900

    180317

---
+++ src/main/java/ThePrincipleOfJava/test01.java
@@ -0,0 +1,32 @@
+package ThePrincipleOfJava;
+
+public class test01 {
+	public static void main(String[] args) {
+		byte b=1;
+		short s = 2;
+		char c = 'A';
+		
+		int finger = 10;
+		long big = 100_000_000_000L;
+		long hex =0xFFFF_FFFF_FFFF_FFFFL;
+		
+		int octNum = 010; // 8진수ㄴ
+		int hexNum = 0x10; // 16진수
+		int bigNum = 0b10; // 2진수
+		
+		System.out.printf("b=%d\n",b);
+		System.out.printf("s=%d\n",s);
+		System.out.printf("c=%c, %d %n",c, (int)c);
+		System.out.printf("finger[%5d]\n = ",finger);
+		System.out.printf("finger[%-5d]\n = ",finger);
+		System.out.printf("finger[%05d]\n = ",finger);
+		
+		System.out.printf("big=%d\n",big);
+		System.out.printf("hex=%#x\n",hex);
+		
+		System.out.printf("octNum =%o,%d\n", octNum, octNum);
+		System.out.printf("hexNum =%x,%d\n", hexNum, hexNum);
+		System.out.printf("octNum =%s,%d\n", bigNum, bigNum);
+		
+	}
+}
---
+++ src/main/java/ThePrincipleOfJava/test02.java
@@ -0,0 +1,13 @@
+package ThePrincipleOfJava;
+
+public class test02 {
+	public static void main(String[] args) {
+		char ch ='A';
+		int code = (int)ch;
+		
+		
+		// char 타입은 문자가 아닌 유니코드가 저장
+		System.out.println(ch);
+		System.out.println(code);
+	}
+}
