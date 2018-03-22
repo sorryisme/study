04d035c6c0e4620979c2a5bb753a60d53f5a6f3f
sorryisme <wivipp39@gmail.com>
2018-03-22 15:51:24 +0900

    180322

--- src/main/java/ThePrincipleOfJava/ch5/Test03.java
+++ src/main/java/ThePrincipleOfJava/ch5/Test03.java
@@ -1,12 +1,25 @@
 // system.arraycopy
-
+// System.arraycopy(num, 0, newNum, 0, num.length) => 기존 배열, 시작인덱스, 복사 받을 배열 , 인덱스 , 복사할 길이
 package ThePrincipleOfJava.ch5;
 
-import java.util.Arrays;
 
 public class Test03 {
     public static void main(String[] args) {
+        char[] abc = { 'A', 'B', 'C', 'D' };
+        char[] num = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        System.out.println(abc);
+        System.out.println(num);
         
+        char[] result = new char[abc.length + num.length];
+        System.arraycopy(abc, 0, result, 0, abc.length);
+        System.arraycopy(num, 0, result,abc.length, num.length);
+        System.out.println(result);
+        
+        System.arraycopy(abc, 0, num, 0, abc.length);
+        System.out.println(num);
+        
+        System.arraycopy(abc, 0, num, 6, 3);
+        System.out.println(num);
         
         
     }
