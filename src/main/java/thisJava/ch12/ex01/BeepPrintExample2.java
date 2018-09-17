88feec6a6179fda91682ddbd46cda53332462f70
sorryisme <wivipp39@gmail.com>
2018-09-17 18:40:29 +0900

    '180917'

---
+++ src/main/java/thisJava/ch12/ex01/BeepPrintExample2.java
@@ -0,0 +1,14 @@
+package thisJava.ch12.ex01;
+
+public class BeepPrintExample2 {
+    public static void main(String[] args) {
+        Runnable beepTask = new BeepTask();
+        Thread thread = new Thread(beepTask);
+        thread.start();
+        
+        for(int i=0; i<5; i++) {
+            System.out.println("띵");
+            try { Thread.sleep(500); } catch (Exception e) {}
+        }
+    }
+}
---
+++ src/main/java/thisJava/ch12/ex01/BeepTask.java
@@ -0,0 +1,16 @@
+package thisJava.ch12.ex01;
+
+import java.awt.Toolkit;
+
+public class BeepTask implements Runnable {
+
+    @Override
+    public void run() {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        for(int i=0; i<5; i++) {
+            toolkit.beep();
+            try { Thread.sleep(500); } catch(Exception e) {}
+        }
+    }
+    
+}
